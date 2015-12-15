f6037dd87a2b079a9de50d7fee7e98c3d3648957
chenqingxiao <chenqingxiao007@iCloud.com>
2015-12-15 14:33:53 +0800

    init

---
+++ CXWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXWeibo
+//
+//  Created by 陈擎霄 on 15/12/15.
+//  Copyright © 2015年 chenqingxiao007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXWeibo
+//
+//  Created by 陈擎霄 on 15/12/15.
+//  Copyright © 2015年 chenqingxiao007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
