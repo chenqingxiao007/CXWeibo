73fde11e50ecc9b928b02c9e395947e0509efe40
chenqingxiao <chenqingxiao007@iCloud.com>
2015-12-15 20:59:40 +0800

    初步初始化CXTabBarViewController

---
+++ CXWeibo/CXKit/Controllers/CXNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CXNavigationController.h
+//  CXWeibo
+//
+//  Created by 陈擎霄 on 15/12/15.
+//  Copyright © 2015年 chenqingxiao007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXNavigationController : UINavigationController
+
+@end
--- CXWeibo/CXTabBarViewController.m
+++ CXWeibo/CXTabBarViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CXTabBarViewController.h"
+#import "CXNavigationController.h"
 
 @interface CXTabBarViewController ()
 
@@ -18,29 +19,9 @@
 - (instancetype)init{
 
     if (self = [super init]) {
-        //b.创建子控制器
-             UIViewController *c1=[[UIViewController alloc]init];
-             c1.view.backgroundColor=[UIColor grayColor];
-             c1.view.backgroundColor=[UIColor greenColor];
-             c1.tabBarItem.title=@"主页";
-             c1.tabBarItem.image=[UIImage imageNamed:@"tab_recent_nor"];
-             c1.tabBarItem.badgeValue=@"123";
-        
-             UIViewController *c2=[[UIViewController alloc]init];
-             c2.view.backgroundColor=[UIColor brownColor];
-             c2.tabBarItem.title=@"消息";
-             c2.tabBarItem.image=[UIImage imageNamed:@"tab_buddy_nor"];
-        
-             UIViewController *c3=[[UIViewController alloc]init];
-             c3.tabBarItem.title=@"搜索";
-             c3.tabBarItem.image=[UIImage imageNamed:@"tab_qworld_nor"];
-        
-             UIViewController *c4=[[UIViewController alloc]init];
-             c4.tabBarItem.title=@"个人";
-             c4.tabBarItem.image=[UIImage imageNamed:@"tab_me_nor"];
-             
-             //c.2第二种方式
-             self.viewControllers=@[c1,c2,c3,c4];
+     
+        //装配控制器
+        [self setupChildViewControllers];
     }
     return self;
 }
@@ -51,19 +32,60 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupChildViewControllers{
+    
+    // 1.首页
+    UIViewController *homeVC = [[UIViewController alloc] init];
+    CXNavigationController *nav1 = [[CXNavigationController alloc] initWithRootViewController:homeVC];
+    nav1.tabBarItem.title = @"主页";
+    nav1.tabBarItem.badgeValue = @"主页";
+    [self addChildViewController:nav1];
+    
+    // 2.消息
+    UIViewController *messageVC = [[UIViewController alloc] init];
+    CXNavigationController *nav2 = [[CXNavigationController alloc] initWithRootViewController:messageVC];
+    nav2.tabBarItem.title = @"消息";
+    [self addChildViewController:nav2];
+    
+    // 3.搜索
+    UIViewController *searchVC = [[UIViewController alloc] init];
+    CXNavigationController *nav3 = [[CXNavigationController alloc] initWithRootViewController:searchVC];
+    nav3.tabBarItem.title = @"搜索";
+    [self addChildViewController:nav3];
+    
+    // 4.消息
+    UIViewController *personVC = [[UIViewController alloc] init];
+    CXNavigationController *nav4 = [[CXNavigationController alloc] initWithRootViewController:personVC];
+    nav4.tabBarItem.title = @"个人";
+    [self addChildViewController:nav4];
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
