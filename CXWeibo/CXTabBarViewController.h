3420eec8ada96de8fd5897e7ca2ee4ffb2772926
chenqingxiao <chenqingxiao007@iCloud.com>
2015-12-15 16:48:44 +0800

    添加pct文件和自定义tabbarController

---
+++ CXWeibo/CXTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CXTabBarViewController.h
+//  CXWeibo
+//
+//  Created by 陈擎霄 on 15/12/15.
+//  Copyright © 2015年 chenqingxiao007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXTabBarViewController : UITabBarController
+
+@end
